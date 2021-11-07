5b3aed608535735c7a8eaed72837c76369563a68
Илья <76221221+Ilysha98@users.noreply.github.com>
2021-11-07 13:48:39 +0300

    Add files via upload

---
+++ HomeWorks4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+		
+
+	int main()
+{
+	system ("chcp 1251 > nul");
+	int s;
+	int k;
+
+	cin >> k;
+
+	s = (4 * k + 3) % 4;
+		cout << s;
+
+
+	
+		
+
+	system("pause>nul");
+	return 0;
+}
